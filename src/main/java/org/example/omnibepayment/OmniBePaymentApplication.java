d01bdd03373185399b2f74ba9974c3140c54b5c2
steamedEggMaster <iron.ch5014@gmail.com>
2025-05-25 02:18:24 +0900

    feat: EntityScan 적용 후 테스트

--- src/main/java/org/example/omnibepayment/OmniBePaymentApplication.java
+++ src/main/java/org/example/omnibepayment/OmniBePaymentApplication.java
@@ -2,12 +2,15 @@ package org.example.omnibepayment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
+@EntityScan(basePackages = "org.example.omnibeuser.entity")
+//@EnableJpaRepositories(basePackages = "org.example.omnibeuser.repository")
 public class OmniBePaymentApplication {
 
     public static void main(String[] args) {
