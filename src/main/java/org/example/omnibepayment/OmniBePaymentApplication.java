363509efd100b82dda90654e427ee723e6dcbf56
steamedEggMaster <iron.ch5014@gmail.com>
2025-05-25 02:22:41 +0900

    feat: EnableJpaRepositories 적용 후 테스트

--- src/main/java/org/example/omnibepayment/OmniBePaymentApplication.java
+++ src/main/java/org/example/omnibepayment/OmniBePaymentApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
-@EntityScan(basePackages = "org.example.omnibepayment.entity")
-//@EnableJpaRepositories(basePackages = "org.example.omnibeuser.repository")
+//@EntityScan(basePackages = "org.example.omnibepayment.entity")
+@EnableJpaRepositories(basePackages = "org.example.omnibepayment.repository")
 public class OmniBePaymentApplication {
 
     public static void main(String[] args) {
